26492faeb7233a92376f7d40e87a1908af5a4f52
manhcaoo <caotienmanh95@gmail.com>
2021-10-09 20:48:11 +0700

    xong 4 bai tap

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
-let inputCelsius;
+///Bai 1
+// let inputToan;
+// let inputLy;
+// let inputHoa;
 
-inputCelsius = prompt("Enter the celsius");
+// inputToan = prompt("Nhap diem toan");
+// inputLy = prompt("Nhap diem ly");
+// inputHoa = prompt("Nhap diem hoa");
 
-let Fahrenheit = (inputCelsius * 9) / 5 + 32;
-document.write(Fahrenheit + "°" + "F");
+// let diemTrungBinh = (inputToan + inputLy + inputHoa) / 3;
+// document.write("diem trung binh la" + " " + diemTrungBinh);
+
+///Bai 2
+
+// let inputCelsius;
+
+// let celsius;
+
+// let Fahrenheit = (inputCelsius * 9) / 5 + 32;
+// document.write(Fahrenheit + "°" + "F");
+
+///Bai 3
+// let inputBanKinh;
+// inputBanKinh = prompt("Nhap ban kinh");
+
+// let dienTich = inputBanKinh * inputBanKinh * 3.14;
+// document.write("dien tich hinh tron la" + " " + dienTich);
+///Bai 4
+let inputChuVi;
+inputChuVi = prompt("Nhap ban kinh");
+
+let ChuVi = inputChuVi * 2 * 3.14;
+document.write("Chu vi hinh tron la" + " " + ChuVi);
